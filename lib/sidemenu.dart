359a31c2f4226bbf5b433df47cd5d07fe83241fd
aufasyaihan <aufa.azzahidi@mhs.unsoed.com>
2023-09-28 23:30:01 +0700

    tambahan

--- lib/sidemenu.dart
+++ lib/sidemenu.dart
@@ -8,93 +8,130 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Sidemenu extends StatelessWidget {
   const Sidemenu({super.key});
 
-  void _deleteUsername() async {
-    // Inisialisasi  share Preferences
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // Simpan Username ke local storage
-    prefs.setString('username', '');
+  Future<void> _logout(BuildContext context) async {
+    // Show a confirmation dialog
+    bool? confirmLogout = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Logout'),
+          content: const Text('Apakah Anda yakin mau keluar?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Tidak'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Ya'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmLogout == true) {
+      // Inisialisasi share Preferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      // Simpan Username ke local storage
+      prefs.setString('username', '');
+
+      // Navigasi Ke Halaman Login
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          Container(
-            color: Colors.lightBlue,
-            child: const DrawerHeader(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Praktikum Pemrograman Mobile',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Ini adalah tugas 3 dari praktikum pemrograman mobile',
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                  ]),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Drawer(
+        child: ListView(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.lightBlue, Colors.white],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: const DrawerHeader(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Praktikum Pemrograman Mobile',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Ini adalah tugas 3 dari praktikum pemrograman mobile',
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                    ]),
+              ),
             ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              // Navigasi ke halaman Home
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const HomePage(),
-                ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.info),
-            title: const Text('About'),
-            onTap: () {
-              // Navigasi ke halaman About
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AboutPage(),
-                ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Profile'),
-            onTap: () {
-              // Navigasi ke halaman About
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Profile(),
-                ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('Logout'),
-            onTap: () {
-              _deleteUsername();
-              // Navigasi ke halaman About
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const LoginPage(),
-                ),
-              );
-            },
-          )
-        ],
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                // Navigasi ke halaman Home
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('About'),
+              onTap: () {
+                // Navigasi ke halaman About
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AboutPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                // Navigasi ke halaman About
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Profile(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              onTap: () {
+                _logout(context);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
